eb12e1481b1cf9ea5a80838a1c9c404dc5f74fe6
YitaoDeng <dengyitao@163.com>
2023-08-14 15:26:04 +0800

    Lab pgtbl:vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void pageprint(pagetable_t pagetable, int level) 
+{
+	for (int i = 0; i < 512; i++) {
+		pte_t pte = pagetable[i];
+		if (pte & PTE_V) {
+			for (int j = 0; j <= level; j++) {
+				if(j!=0) printf(" ");
+				printf("..");
+			}
+			printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+			if (level < 2 && (pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+				pageprint((pagetable_t)PTE2PA(pte), level + 1);
+			}
+		}
+	}
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  pageprint(pagetable, 0);
+}
